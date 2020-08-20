1a3c49c7a1acb825a06e23a42260aaee2527ae5a
pavanbhat1999 <pavan.17.becs@acharya.ac.in>
2020-08-20 18:11:23 +0530

    button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-first-angular-app';
-  
+  index: number = -1;
+  voteClick(id: number) {
+    if (this.index === id) {
+      this.index = -1;
+    }
+    else {
+      this.index = id;
+    }
+  }
+
+  isSelected(id) {
+    return this.index === id;
+  }
+  isDisableVote(id){
+    if(this.index === id)
+    {
+      return 0;
+    }
+    if (this.index !== -1){
+      return 1;
+    }
+
+    else{
+      return 0;
+    }
+  }
 }
