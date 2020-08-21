1c5c47b057417e4aaf50d72f0798b8653bcde6f3
pavanbhat1999 <pavan.17.becs@acharya.ac.in>
2020-08-21 19:56:45 +0530

    new commit

--- src/app/movie-display/movie-display.component.ts
+++ src/app/movie-display/movie-display.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-movie-display',
@@ -11,31 +11,5 @@ export class MovieDisplayComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  index: number = -1;
-  voteClick(id: number, event) {
-    if (this.index === id) {
-      this.index = -1;
-      let myvar = event.target;
-      myvar.innerText = 'VOTE';
-    } else {
-      this.index = id;
-      let myvar = event.target;
-      myvar.innerText = 'UNVOTE';
-    }
-  }
-
-  isSelected(id) {
-    return this.index === id;
-  }
-
-  isDisableVote(id) {
-    if (this.index === id) {
-      return 0;
-    }
-    if (this.index !== -1) {
-      return 1;
-    } else {
-      return 0;
-    }
-  }
+  @Input() movie;
 }
